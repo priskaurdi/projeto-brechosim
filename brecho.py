3bf9708e977a87431ca50abac13a56d7de510305
Priscilla Sequetin <55082190+priskaurdi@users.noreply.github.com>
2024-05-10 06:36:34 -0300

    flask-10.05.2024

--- brecho.py
+++ brecho.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, session, flash, url_for
 
 # Definição da classe Catalogo para representar os itens do catálogo
 class Catalogo:
@@ -10,6 +10,39 @@ class Catalogo:
         self.descricao = descricao
         self.preco = preco
 
+# Definição da classe Usuário
+class Usuario:
+    def __init__(self, cdCliente, nmCliente, cpf, celular, endereco, numero, compEnd, 
+                 bairro, cidade, estado, cep, login, senha):
+        self.cdCliente = cdCliente
+        self.nmCliente = nmCliente
+        self.cpf = cpf
+        self.celular = celular
+        self.endereco = endereco
+        self.numero = numero
+        self.compEnd = compEnd
+        self.bairro = bairro
+        self.cidade = cidade
+        self.estado = estado
+        self.cep = cep
+        self.login = login
+        self.senha = senha
+
+usuario01 = Usuario("0001", "Daniel", "12345678900", "11912345678", "Rua Mantua", "15", "Casa", "Lisboa", 
+                    "Santos", "CE", "08646780", "dani.dani", "admin")
+usuario02 = Usuario("0002", "Patrick", "12345678900", "11912345678", "Rua Mantua", "15", "Casa", "Lisboa", 
+                    "Santos", "CE", "08646780", "patrick.sponge", "1234")
+usuario03 = Usuario("0003", "Xavier", "12345678900", "11912345678", "Rua Mantua", "15", "Casa", "Lisboa", 
+                    "Santos", "CE", "08646780", "xavi.mente", "7968")
+
+usuarios = {
+    usuario01.login : usuario01,
+    usuario02.login : usuario02,
+    usuario03.login : usuario03
+}
+
+
+# Criando o app
 app = Flask(__name__)
 
 app.secret_key = 'brechosimmodasustentavel'
@@ -26,7 +59,10 @@ def iniciarBrecho():
 # Rota para renderizar a página da coleção completa
 @app.route('/colecao')
 def verColecao():
-    return render_template('colecao.html', fotos=catalogo)
+    if session['usuario_logado'] == None or 'usuario_logado' not in session:
+        return redirect(url_for('login'))
+    else:   
+        return render_template('colecao.html', fotos=catalogo)
 
 
 # Rota para renderizar a página de login
@@ -38,18 +74,35 @@ def login():
 # Rota para autenticar o login
 @app.route('/autenticar', methods=['POST'])
 def autenticar():
-    if request.form['txtSenha'] == 'admin':
+    if request.form['txtLogin'] in usuarios:
+
+        usuarioEncontrado = usuarios[request.form['txtLogin']]
+
+        if request.form['txtSenha'] == usuarioEncontrado.senha:
         
-        session['usuario_logado'] = request.form['txtLogin']
+            session['usuario_logado'] = request.form['txtLogin']
         
-        return redirect('/')
+            flash(f"Seja bem-vindo, {usuarioEncontrado.nome}!")
+
+            return redirect(url_for('iniciarBrecho'))
+        else:
+            flash("Senha inválida!")
+
+            return redirect(url_for('login')) 
+    
     else:
-        return redirect('/login')
+
+        flash("Usuário ou Senha inválida")
+
+        return redirect(url_for('login'))
 
 # Rota para sair
 @app.route('/agendar')
 def agendar():
-    return render_template('agenda.html')
+    if session['usuario_logado'] == None or 'usuario_logado' not in session:
+        return redirect(url_for('login'))
+    else:   
+        return render_template('agenda.html')
 
 
 # Rota para sair
@@ -57,28 +110,32 @@ def agendar():
 def sair():
     session['usuario_logado'] = None
 
-    return redirect('/login')
+    return redirect(url_for('login'))
 
 
 # Rota para renderizar a página de cadastro e processar o formulário
 @app.route('/cadastrar', methods=['GET', 'POST'])
 def cadastrar():
-    if request.method == 'POST':
-        # Recupera os dados do formulário
-        foto = request.files['foto']
-        idnome = request.form['idnome']
-        descricao = request.form['descricao']
-        preco = request.form['preco']
-        
-        # Salva a imagem no diretório "static/imagens"
-        caminho_imagem = salvar_imagem(foto)
-        
-        # Cria uma nova instância da classe Catalogo com os dados do formulário
-        novo_item = Catalogo(caminho_imagem, idnome, descricao, preco)
-        
-        # Adiciona o novo item à lista de catalogo
-        catalogo.append(novo_item)
-        
+
+    if session['usuario_logado'] == None or 'usuario_logado' not in session:
+        return redirect(url_for('login'))
+    else:   
+        if request.method == 'POST':
+            # Recupera os dados do formulário
+            foto = request.files['foto']
+            idnome = request.form['idnome']
+            descricao = request.form['descricao']
+            preco = request.form['preco']
+            
+            # Salva a imagem no diretório "static/imagens"
+            caminho_imagem = salvar_imagem(foto)
+            
+            # Cria uma nova instância da classe Catalogo com os dados do formulário
+            novo_item = Catalogo(caminho_imagem, idnome, descricao, preco)
+            
+            # Adiciona o novo item à lista de catalogo
+            catalogo.append(novo_item)
+            
     # Renderiza a página de cadastro
     return render_template('cadastra_catalogo.html')
 
